322fb98e16f8e33d173b87dbb4aeae606eb06ef3
Pivotal Tracker <labs-tracker-team@pivotal.io>
2016-05-27 14:40:15 -0600

    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbblah

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,37 @@
 (function() {
   'use strict';
 
+  // Insert injected weather forecast here
+  var initialWeatherForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
   var app = {
     isLoading: true,
+    hasRequestPending: false,
     visibleCards: {},
     selectedCities: [],
     spinner: document.querySelector('.loader'),
@@ -38,8 +67,8 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
-    app.toggleAddDialog(false);
     app.saveSelectedCities();
+    app.toggleAddDialog(false);
   });
 
   document.getElementById('butAddCancel').addEventListener('click', function() {
@@ -125,6 +154,23 @@
   app.getForecast = function(key, label) {
     var url = 'https://publicdata-weather.firebaseio.com/';
     url += key + '.json';
+    if ('caches' in window) {
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function(json) {
+            // Only update if the XHR is still pending, otherwise the XHR
+            // has already returned and provided the latest data.
+            if (app.hasRequestPending) {
+              console.log('[App] Forecast Updated From Cache');
+              json.key = key;
+              json.label = label;
+              app.updateForecastCard(json);
+            }
+          });
+        }
+      });
+    }
+    app.hasRequestPending = true;
     // Make the XHR to get the data, then update the card
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
@@ -134,6 +180,7 @@
           response.key = key;
           response.label = label;
           app.hasRequestPending = false;
+          console.log('[App] Forecast Updated From Network');
           app.updateForecastCard(response);
         }
       }
@@ -150,43 +197,26 @@
     });
   };
 
-  var initialWeatherForecast = {
-    key: 'newyork',
-    label: 'New York, NY',
-    currently: {
-      time: 1453489481,
-      summary: 'Clear',
-      icon: 'partly-cloudy-day',
-      temperature: 52.74,
-      apparentTemperature: 74.34,
-      precipProbability: 0.20,
-      humidity: 0.77,
-      windBearing: 125,
-      windSpeed: 1.52
-    },
-    daily: {
-      data: [
-        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
-      ]
-    }
-  };
-
-  // app.updateForecastCard(initialWeatherForecast);
-
-  // Save list of cities to localStorage.
+  // Save list of cities to localStorage, see note below about localStorage.
   app.saveSelectedCities = function() {
     var selectedCities = JSON.stringify(app.selectedCities);
+    // IMPORTANT: See notes about use of localStorage.
     localStorage.selectedCities = selectedCities;
   };
 
-  app.selectedCities = localStorage.selectedCities;
+  /*****************************************************************************
+   *
+   * Code required to start the app
+   *
+   * NOTE: To simplify this getting started guide, we've used localStorage.
+   *   localStorage is a syncronous API and has serious performance
+   *   implications. It should not be used in production applications!
+   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+   *
+   ****************************************************************************/
 
+  app.selectedCities = localStorage.selectedCities;
   if (app.selectedCities) {
     app.selectedCities = JSON.parse(app.selectedCities);
     app.selectedCities.forEach(function(city) {
@@ -200,27 +230,11 @@
     app.saveSelectedCities();
   }
 
+  // Add feature check for Service Workers here
   if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/service-worker.js')
-           .then(function() { console.log('Service Worker Registered'); });
-  }
-
-  if ('caches' in window) {
-    caches.match(url).then(function(response) {
-      if (response) {
-        response.json().then(function(json) {
-          // Only update if the XHR is still pending, otherwise the XHR
-          // has already returned and provided the latest data.
-          if (app.hasRequestPending) {
-            console.log('updated from cache');
-            json.key = key;
-            json.label = label;
-            app.updateForecastCard(json);
-          }
-        });
-      }
-    });
+    navigator.serviceWorker
+             .register('/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
   }
 
 })();
